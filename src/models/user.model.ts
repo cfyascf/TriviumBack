8861820b99caec20a586cd3644ba28ca4ae8b841
YasminTrembulack <yasmin.agostinho012@gmail.com>
2024-09-17 15:23:55 -0300

    fixed models

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,14 +4,12 @@ interface IUser extends Document {
     fullname: string,
     email: string,
     password: string,
-    role: number
 }
 
 const UserSchema: Schema<IUser> = new Schema({
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false },
-    role: { type: Number, required: true }
 });
 
 const User = mongoose.model<IUser>('User', UserSchema);
